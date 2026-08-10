0db5a4b0d3cc1be59e0080d428dbf46b48cbc028
Harishparley <harishparleyhp@gmail.com>
2026-08-10 20:16:40 +0530

    count of unique element in array problem

---
+++ Arrays/uniquElementArray.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class uniquElementArray {
+    public static void main(String[] args) {
+        int []arr = {1,1,2,2,2,3,3,4,4,5,5,6};
+
+        int i = 0;
+        for(int j=1; j<arr.length; j++){
+            if(arr[i]!=arr[j]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        System.out.println(i+1);
+    }
+}
