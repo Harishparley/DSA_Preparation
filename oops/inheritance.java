cc7de9c0c2e7b08c937b9e3055299065287b4846
Harishparley <harishparleyhp@gmail.com>
2026-08-15 21:18:55 +0530

    complete inheritance

---
+++ oops/inheritance.java
@@ -0,0 +1,31 @@
+public class inheritance{
+    public static void main(String[] args) {
+        
+        // class A{
+        //     int a=1;
+        //     int b = 3;
+
+        // }
+
+        // class B extends A{
+        //     int c = 6;
+        // }
+
+        // class C extends B{
+        //     int d = 7;
+
+        //     void show(){
+        //         System.out.println("done");
+        //     }
+        // }
+
+        // C obj =new C();
+        // obj.show();
+        // System.out.println(obj.b);
+
+
+    
+
+
+     }
+}
