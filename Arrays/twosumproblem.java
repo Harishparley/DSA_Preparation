bb2036d055125f64fb5130b8c9a05e4d2f093f94
Harishparley <harishparleyhp@gmail.com>
2026-08-18 13:58:57 +0530

    update the code

--- Arrays/twosumproblem.java
+++ Arrays/twosumproblem.java
@@ -1,4 +1,4 @@
- import java.util.Arrays;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class twosumproblem {
@@ -22,6 +22,23 @@ public class twosumproblem {
         return new int[] {};
 
     }
+
+       // public int[] twoSum(int[] nums, int target) {
+    //     int n = nums.length;
+    //     for(int i=0; i<n; i++){
+    //         for(int j=i+1; j<n; j++){
+    //             if(nums[i]+nums[j]==target){
+    //                 return new int[]{i,j};
+    //             }
+    //         }
+    //     }
+    //  return new int[]{};
+
+    // }
     
 }
+
+
+ 
+
 
