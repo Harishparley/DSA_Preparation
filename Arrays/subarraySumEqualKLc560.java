eb4825592dd4b40a8bbf3294cb05798acb583683
Harishparley <harishparleyhp@gmail.com>
2026-08-22 23:35:05 +0530

    solve this with optimal approach using hashmap

--- Arrays/subarraySumEqualKLc560.java
+++ Arrays/subarraySumEqualKLc560.java
@@ -2,27 +2,46 @@ import java.util.HashMap;
 
 public class subarraySumEqualKLc560 {
     public static void main(String[] args) {
-        int []arr = {1,2,3};
-        int k =3;
-       System.out.println(subarraySum(arr, k));
+        int[] arr = { 1, 2, 3 };
+        int k = 3;
+        System.out.println(subarraySumnew(arr, k));
     }
-    public static int subarraySum(int[] arr, int k) {
-           int count = 0;
 
-    for (int i = 0; i < arr.length; i++) {
+    // public static int subarraySum(int[] arr, int k) {
+    // int count = 0;
 
-        int sum = 0;
+    // for (int i = 0; i < arr.length; i++) {
 
-        for (int j = i; j < arr.length; j++) {
+    // int sum = 0;
 
-            sum += arr[j];
+    // for (int j = i; j < arr.length; j++) {
 
-            if (sum == k) {
-                count++;
-            }
+    // sum += arr[j];
+
+    // if (sum == k) {
+    // count++;
+    // }
+    // }
+    // }
+
+    // return count;
+    // }
+
+    public static int subarraySumnew(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
+        int count = 0;
+        map.put(0, 1);
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+            int remove = prefixSum - k;
+            if (map.containsKey(remove)) {
+                count += map.get(remove);
+            } 
+                map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+            
         }
-    }
 
-    return count;
+        return count;
     }
 }
