6b1aaa2343f0c75c2ad20a8d56bb4d56b20e0096
Harishparley <harishparleyhp@gmail.com>
2026-08-22 22:43:35 +0530

    solve subarray sum equal k lc560 problem

---
+++ Arrays/subarraySumEqualKLc560.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class subarraySumEqualKLc560 {
+    public static void main(String[] args) {
+        int []arr = {1,2,3};
+        int k =3;
+       System.out.println(subarraySum(arr, k));
+    }
+    public static int subarraySum(int[] arr, int k) {
+           int count = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+
+        int sum = 0;
+
+        for (int j = i; j < arr.length; j++) {
+
+            sum += arr[j];
+
+            if (sum == k) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+    }
+}
